15a47498baa3f4761910f0225046efbeb0b9dfb4
Ben Christian Aceveda <benchristianaceveda23@gmail.com>
2024-12-01 21:58:02 +0800

    changed ulit

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
        <input type="submit" class="btn" value="Sign Up" name="signUp">
       </form>
       <div class="links">
-        <p>Already Have Account?</p>
+        <p>may account kana?</p>
         <button id="signInButton">Sign In</button>
       </div>
     </div>
